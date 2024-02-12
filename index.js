8e9604f28ed43a340829ff270fabf9cfe34ff698
Dennis <dennismusembi344@gmail.com>
2024-02-12 13:59:00 +0300

    done2

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-// Write your solution in this file!
+// // Write your solution in this file!
+// const employee={
+//     name:"Sam",
+//     streetAddress:"11 Broadway",
+// };
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     const newObj={...employee,"name":"Sam","streetAddress":"11 Broadway"}
+//     return newObj
+// }
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee["streetAddress"]="12 Broadway"
+//     return employee
+// }
+// function deleteFromEmployeeByKey(employee, key){
+//     const newEmployee={...employee[key]}
+//     delete newEmployee[key]
+//     return newEmployee
+
+// }
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//     delete employee[key]
+//     return employee
+// }
 const employee={
-    name:"Sam",
-    streetAddress:"11 Broadway",
-};
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newObj={...employee,"name":"Sam","streetAddress":"11 Broadway"}
+    name:"Janeee",
+    streetAddress:"Ndenderu",
+}
+function updateEmployeeWithKeyAndValue(employee,key,value){
+    const newObj={
+        ...employee,
+        [key]:value
+    }
     return newObj
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee["streetAddress"]="12 Broadway"
-    return employee
-}
-function deleteFromEmployeeByKey(employee, key){
-    const newEmployee={...employee[key]}
-    delete newEmployee[key]
-    return newEmployee
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+     employee[key]=value
+     return employee
 
 }
-function destructivelyDeleteFromEmployeeByKey(employee, key){
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee={
+        ...employee
+    }
+    delete newEmployee[key]
+    return newEmployee
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key){
     delete employee[key]
     return employee
 }
